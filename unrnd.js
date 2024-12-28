706a51be9376181dba25a57f1eca137602b075f1
Das Ulicorn <>
2024-12-28 20:23:10 +0100

    refactor

--- unrnd.js
+++ unrnd.js
@@ -1,43 +1,48 @@
-function unfuck(o){
+function unfuckElement(x){
+    switch (x.type){
+    case 'ad':
+    case 'piano':
+	return '';
+
+    case 'header':
+	return `<h2>${x.text}</h2>`;
+
+    case 'text':
+	return `<p>${x.text}</p>`;
+
+    case 'rawHtml':
+	return `<p>${x.html}</p>`;
+
+    case 'image':
+	return `<figure><img src="${x.imageInfo.src}" /><figcaption>${x.imageInfo.caption}</figcaption></figure>`;
+
+    case 'list':
+	var tag = (x.list.isOrdered? 'ol':'ul'),
+	    mkli = a => `<li>${a.text}</li>`;
+	return `<${tag}>${x.list.items.map(mkli).join('')}</${tag}>`;
+
+    case 'video':
+	var mksrc = s => `<source src="${s.url}" />`;
+	return `<video controls  poster="${x.videoInfo.promoImage.src}" preload="metadata">
+                 <img src="${x.videoInfo.promoImage.src}" />
+                 ${x.videoInfo.streams.reverse().map(mksrc).join('')}</video>`;
+
+    default:
+	return `<details><summary>Unknown type <code>${x.type}</code></summary><pre>${JSON.stringify(x,null,4)}</pre></details>`;
+
+    }
+};
+
+
+function unfuckAll(o){
     document.querySelectorAll('style,link[rel="stylesheet"]').forEach(item => item.remove());
-    document.body.innerHTML=`<link rel="stylesheet" href="https://das-ulicorn.github.io/unrnd/unrnd.css" /><h1>${o.headline}</h1><p><b>${o.subHeadline}</b></p><figure><img src="${o.promoImage.src}" /><figcaption>${o.promoImage.caption}</figcaption></figure>`;
-    o.elements.forEach((x) => {
-	switch (x.type){
-	case 'ad':
-	case 'piano':
-	    break;
-	case 'header':
-	    document.body.innerHTML+=`<h2>${x.text}</h2>`;
-	    break;
-	case 'text':
-	    document.body.innerHTML+=`<p>${x.text}</p>`;
-	    break;
-	case 'rawHtml':
-	    document.body.innerHTML+=`<p>${x.html}</p>`;
-	    break;
-	case 'image':
-	    document.body.innerHTML+=`<figure><img src="${x.imageInfo.src}" /><figcaption>${x.imageInfo.caption}</figcaption></figure>`;
-	    break;
-	case 'list':
-	    document.body.innerHTML+= (x.list.isOrdered? '<ol>':'<ul>');
-	    x.list.items.forEach((a) => {
-		document.body.innerHTML += `<li>${a.text}</li>`;
-	    });
-	    document.body.innerHTML += (x.list.isOrdered? '</ol>':'</ul>');
-	    break;
-	case 'video':
-	    document.body.innerHTML += `<video controls poster="${x.videoInfo.promoImage.src}" preload="metadata"><img src="${x.videoInfo.promoImage.src}" />`;
-	    
-	    x.videoInfo.streams.forEach((s) => {
-		document.body.innerHTML += `<source src="${s.url}">`;
-	    });
-	    document.body.innerHTML += `</video>`;
-	    break;
-	default:
-	    document.body.innerHTML += `<details><summary>Unknown type <code>${x.type}</code></summary><pre>${JSON.stringify(x,null,4)}</pre></details>`;
-	}
-    });
+    document.body.innerHTML=
+	`<link rel="stylesheet" href="https://das-ulicorn.github.io/unrnd/unrnd.css" />
+         <h1>${o.headline}</h1>
+         <p><b>${o.subHeadline}</b></p>
+         <figure><img src="${o.promoImage.src}" /><figcaption>${o.promoImage.caption}</figcaption></figure>
+         ${o.elements.map(unfuckElement).join("\n")}`;
 }
 
 eval(document.getElementById('fusion-metadata').innerHTML);
-unfuck(Fusion.globalContent);
+unfuckAll(Fusion.globalContent);
